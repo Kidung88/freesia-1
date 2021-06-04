1a46053e71aa8017e1d1efb1a1c31ccbea355e40
Kidung Arya Pikatan <kidungaryaa@gmail.com>
2021-06-04 20:05:19 +0700

    Tambah semua file

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","freesia2");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
+
+?>
